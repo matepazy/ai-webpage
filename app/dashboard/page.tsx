ea4e3f7ac9149e9fb323c4cf95a1fa821ab4a5c9
matepazy <82311916+matepazy@users.noreply.github.com>
2024-11-14 20:43:38 +0100

    asd

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { AppSidebar } from "@/components/sidebar";
+import { Sidebar } from "@/components/ui/sidebar";
+
+export default function Dashboard() {
+    return(
+        <main>
+            <AppSidebar />
+        </main>
+    )
+}
